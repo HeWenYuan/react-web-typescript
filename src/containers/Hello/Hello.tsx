30637b087cd0fedf9f1a3ff61c0354e7616a9952
jokerdrake <joker92919@gmail.com>
2017-03-03 20:35:36 +0800

    update: add react-router

---
+++ src/containers/Hello/Hello.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default class Hello extends React.Component<undefined, undefined> {
+  render () {
+    return (
+      <div>Hello Page.</div>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
 import {createStore} from 'redux';
 import { Provider } from 'react-redux';
 import * as ReactDOM from 'react-dom';
 import reducer from './redux/reducer';
+
+
 import App from './containers/App/App';
+import Hello from './containers/Hello/Hello';
+
 
 let store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}></Route>
+      <Route path="/hello" component={Hello}></Route>
+    </Router>
   </Provider>
   ,
   document.getElementById("content")
